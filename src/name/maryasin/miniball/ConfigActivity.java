7c6240a934677394449b4abf5f7d4f8c7d304921
Semyon Maryasin <simeon@maryasin.name>
2014-01-01 06:40:32 +0400

    Добавил XML и шаблон активити для настроек
    Используется вариант для ActionBarSherlock. Следует от него избавиться,
    перейдя на min-Android4.

---
+++ src/name/maryasin/miniball/ConfigActivity.java
@@ -0,0 +1,24 @@
+package name.maryasin.miniball;
+
+import java.util.List;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+
+public class ConfigActivity extends SherlockPreferenceActivity {
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+			addPreferencesFromResource(R.xml.settings);
+		}
+	}
+	
+	@Override
+	public void onBuildHeaders(List<Header> target) {
+		//loadHeadersFromResource(R.xml.config_headers, target);
+	}
+}
