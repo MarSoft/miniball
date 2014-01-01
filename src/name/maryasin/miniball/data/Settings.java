45fc51579ebd631871725f604fc7c714271bd803
Semyon Maryasin <simeon@maryasin.name>
2014-01-01 08:30:11 +0400

    Settings: reinit DataManager on rootpath change

--- src/name/maryasin/miniball/data/Settings.java
+++ src/name/maryasin/miniball/data/Settings.java
@@ -1,18 +1,16 @@
 package name.maryasin.miniball.data;
 
+import java.io.IOException;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
 
 public class Settings implements OnSharedPreferenceChangeListener {
 	private static final String KEY_ROOTPATH = "cfg_rootPath";
-	
-	private SharedPreferences sharedPrefs;
-	
-	private Settings(Context context) {
-		sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-	}
 	
 	private static Settings instance = null;
 	public static void init(Context context) {
@@ -24,12 +22,28 @@ public class Settings implements OnSharedPreferenceChangeListener {
 		return instance;
 	}
 
+	private SharedPreferences sharedPrefs;
+	private Context context;
+	private Settings(Context context) {
+		this.context = context;
+		this.sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+	}
+	
 	public String getRootPath() {
 		return sharedPrefs.getString(KEY_ROOTPATH, "");
 	}
 	
 	@Override
-	public void onSharedPreferenceChanged(SharedPreferences arg0, String arg1) {
-		// TODO: update data when rootpath is changed
+	public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+		if(key.equals(KEY_ROOTPATH)) {
+			try {
+				DataManager.initDanceList();
+			} catch (IOException e) {
+				Log.e("DanceListFragment", "Ошибка инициализации DataManager", e);
+				Toast.makeText(context, "Не удалось инициализировать данные!\nОшибка: "+e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+				return;
+			}
+			// TODO: update interface
+		}
 	}
 }
