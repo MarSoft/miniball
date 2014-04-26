e5bd36e2b6d56ccd5c4e22d3895eacd5c3fec11b
Semyon Maryasin <simeon@maryasin.name>
2014-04-27 00:22:36 +0400

    PlayerService: added basic notification routines

--- src/name/maryasin/miniball/player/PlayerService.java
+++ src/name/maryasin/miniball/player/PlayerService.java
@@ -1,17 +1,54 @@
 package name.maryasin.miniball.player;
 
+import name.maryasin.miniball.R;
 import name.maryasin.miniball.data.*;
 
-import android.app.Service;
+import android.app.*;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.*;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.io.File;
 
 public class PlayerService extends Service {
+	private NotificationManager notifMgr;
+
+	private static final int NOTIF_ID = R.string.local_service_id;
+
+	public static final String TAG = "PlayerService";
+
+	@Override
+	public void onCreate() {
+		notifMgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+		// show persistent notification
+		showNotification();
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.i(TAG, "Received start id "+startId+": "+intent);
+		// Keep running until explicit stop
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		// hide persistent notification
+		notifMgr.cancel(NOTIF_ID);
+		// Debug, FIXME
+		Toast.makeText(this, "PlayerService stopped", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO
 		return null;
+	}
+
+	private void showNotification() {
+		// TODO
 	}
 }
