767e661b8232810a33953b054a308eaeac1fef3a
Semyon Maryasin <simeon@maryasin.name>
2014-04-27 00:08:53 +0400

    Added PlayerService template

---
+++ src/name/maryasin/miniball/player/PlayerService.java
@@ -0,0 +1,17 @@
+package name.maryasin.miniball.player;
+
+import name.maryasin.miniball.data.*;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.*;
+
+import java.io.File;
+
+public class PlayerService extends Service {
+	public IBinder onBind(Intent intent) {
+		// TODO
+		return null;
+	}
+}
