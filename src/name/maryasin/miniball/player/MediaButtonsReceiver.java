3d8d31230b46f25d8f08492ae9f9cc6e7e4e37d8
Semyon Maryasin <simeon@maryasin.name>
2014-04-28 14:36:06 +0400

    MBR: added handleKey

--- src/name/maryasin/miniball/player/MediaButtonsReceiver.java
+++ src/name/maryasin/miniball/player/MediaButtonsReceiver.java
@@ -6,9 +6,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.util.Log;
+import android.view.KeyEvent;
 
 public class MediaButtonsReceiver extends BroadcastReceiver {
 	private static final String TAG = "MediaButtonsReceiver";
+
+	private static boolean handleKey(Context ctx, KeyEvent evt) {
+		Log.d(TAG, "Processing key: "+evt);
+
+		if(evt == null)
+			return false;
+
+		switch(evt.getKeyCode()) {
+			case KeyEvent.KEYCODE_HEADSETHOOK:
+		}
+		return false;
+	}
 
 	public static void register(Context ctx) {
 		AudioManager am = (AudioManager)ctx.getSystemService(Context.AUDIO_SERVICE);
@@ -23,6 +36,12 @@ public class MediaButtonsReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context ctx, Intent intent) {
-		Log.d(TAG, "Received button event: " + intent);
+		Log.d(TAG, "Received event: " + intent);
+		if(intent != null && Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
+			// yep, everything is alright, this is our intent
+			KeyEvent evt = intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+			if(handleKey(ctx, evt) && isOrderedBroadcast())
+				abortBroadcast();
+		}
 	}
 }
