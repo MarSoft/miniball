84b60e628abe36f47253c0e72e88672034967f1a
Semyon Maryasin <simeon@maryasin.name>
2014-04-28 14:23:44 +0400

    Added MediaButtonsReceiver

---
+++ src/name/maryasin/miniball/player/MediaButtonsReceiver.java
@@ -0,0 +1,12 @@
+package name.maryasin.miniball.player;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MediaButtonsReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context ctx, Intent intent) {
+		//
+	}
+}
