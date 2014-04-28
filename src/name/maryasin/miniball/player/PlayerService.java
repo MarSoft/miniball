61f88171c345ff55c14af44ca5b3ae7ae6a617c4
Semyon Maryasin <simeon@maryasin.name>
2014-04-28 05:42:53 +0400

    PS: initial actions

--- src/name/maryasin/miniball/player/PlayerService.java
+++ src/name/maryasin/miniball/player/PlayerService.java
@@ -5,12 +5,16 @@ import name.maryasin.miniball.R;
 
 import android.app.*;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.*;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 public class PlayerService extends Service {
+	public static final String ACTION_ENQUEUE = "name.maryasin.miniball.action.ENQUEUE";
+	public static final String ACTION_STOP = "name.maryasin.miniball.action.STOP";
+
 	private NotificationManager notificationMgr;
 
 	private static final int NOTIFICATION_ID = R.string.local_service_id;
@@ -29,7 +33,7 @@ public class PlayerService extends Service {
 	public void onDestroy() {
 		// hide persistent notification
 		notificationMgr.cancel(NOTIFICATION_ID);
-		// TODO stop music
+		// TODO: stop music if still playing
 		// Debug, FIXME
 		Toast.makeText(this, "PlayerService stopped", Toast.LENGTH_SHORT).show();
 	}
@@ -37,9 +41,18 @@ public class PlayerService extends Service {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.i(TAG, "Received start id "+startId+": "+intent);
-		// TODO: handle intent, if needed
-		if(intent == null) {
-			// We were restarted by system. TODO: resume playback form stored position
+		if(intent != null) {
+			String action = intent.getAction();
+			if(action.equals(ACTION_ENQUEUE)) {
+				Uri track = intent.getData();
+				Log.i(TAG, "Enqueue track: "+track);
+			} else if(action.equals(ACTION_STOP)) {
+				Log.i(TAG, "Stop playback");
+			} else {
+				Log.w(TAG, "Unknown action: "+action);
+			}
+		} else {
+			// We were restarted by system. TODO: resume playback form stored position?
 		}
 
 		// Keep running until explicit stop
